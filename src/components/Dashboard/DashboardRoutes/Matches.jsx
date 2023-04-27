3329770005511da2d942adc6399b30682733834f
Sosaristic <sundayomena9@gmail.com>
2023-04-28 00:01:58 +0100

    created create button

--- src/components/Dashboard/DashboardRoutes/Matches.jsx
+++ src/components/Dashboard/DashboardRoutes/Matches.jsx
@@ -5,9 +5,10 @@ export default function Matches() {
   return (
     <div>
 
-      <div className='flex relative flex-wrap justify-evenly  gap-4 px-8'>
+      <div className='flex relative flex-wrap justify-evenly  gap-4 px-8 pb-20'>
       {Array.from({length:8}, (v, id)=>  <TeamsCard key={id}/>)}
       </div>
+      <button className='create-btn-shadow fixed flex items-center justify-center text-[.8rem] lg:text-[1rem] font-[600]  bottom-[15%] right-[10%] lg:bottom-[10%] w-[3rem] h-[3rem] sm:w-[5rem] sm:h-[5rem]  rounded-full p-8 bg-primary'>Create <br /> Match</button>
       </div>
   )
 }
