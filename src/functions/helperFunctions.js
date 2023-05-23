1fd69c585d9d4f7150adcfa3ff05c21da62af84f
Sosaristic <sundayomena9@gmail.com>
2023-05-23 07:53:24 +0100

    added sliding feature when deleting player

--- src/functions/helperFunctions.js
+++ src/functions/helperFunctions.js
@@ -1,3 +1,13 @@
-export function getPlayerDetails(id, squadData){
-    return squadData?.find((item)=>item.id == id)
+export function getPlayerDetails(id, squadData) {
+  return squadData?.find((item) => item.id == id);
+}
+
+export function modifyPlayerDetails(id, squadData, key, value) {
+  const newSquadData = squadData?.map((player) => {
+    if (player.id === id) {
+      return { ...player, [key]: value };
+    }
+    return player;
+  });
+  return newSquadData
 }
