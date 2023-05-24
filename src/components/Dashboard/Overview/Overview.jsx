475b691b5e926247e133c7483b2063ad52e76974
Sosaristic <sundayomena9@gmail.com>
2023-05-24 08:55:43 +0100

    overview page design

--- src/components/Dashboard/Overview/Overview.jsx
+++ src/components/Dashboard/Overview/Overview.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import {DHero} from ".."
+import TeamFormCard from './TeamFormCard'
 
 export default function Overview() {
   return (
-    <section>
+    <section className='bg-black'>
         <DHero />
+        <section className='flex'>
+        <TeamFormCard />
+        </section>
     </section>
   )
 }
---
+++ src/components/Dashboard/Overview/TeamFormCard.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function(){
+    return (
+        <>
+        <div className="basis-[45%] bg-gray-600">
+            <p>Team form</p>
+            <div className="circle-wrap">
+               <span>0%</span>
+            </div>
+        </div>
+        </>
+    )
+}
