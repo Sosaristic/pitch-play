1a4d30ae0f90ed2635e8027f275b92056b21da5a
Sosaristic <sundayomena9@gmail.com>
2023-06-24 02:36:47 +0100

    fixed error on my team route

--- src/components/Dashboard/MyTeam/MyTeam.jsx
+++ src/components/Dashboard/MyTeam/MyTeam.jsx
@@ -4,24 +4,24 @@ import { CreateTeam, MyTeamNav } from "../../Dashboard";
 import { useAppContext } from "../../../context/AppContext";
 import { UserHasNoTeam } from "../../UI";
 export default function MyTeam() {
-const {userHasTeam} = useAppContext()
+  const { userHasTeam } = useAppContext();
 
- if(!userHasTeam){
-  return <div><UserHasNoTeam /></div>
- }
+  if (!userHasTeam) {
+    return (
+      <div>
+        <UserHasNoTeam />
+      </div>
+    );
+  }
   return (
     <div className="w-full relative">
-     
-     {haveTeam && <div className="sticky top-0 lg:top-0 z-[100]"><MyTeamNav /></div>} 
+      <div className="sticky top-0 lg:top-0 z-[100]">
+        <MyTeamNav />
+      </div>
 
-      {haveTeam ? (
-        <div className="px-2 pb-[6.5rem] lg:pb-1 overflow-hidden">
-          
-          <Outlet />
-        </div>
-      ) : (
-        <Navigate to="/dashboard/my-team/create-team" replace/>
-      )}
+      <div className="px-2 pb-[6.5rem] lg:pb-1 overflow-hidden">
+        <Outlet />
+      </div>
     </div>
   );
 }
